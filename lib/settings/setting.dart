c2b1d6c4ce3b7ffef1f82fe8bf25ba80e306b156
JangJuMan <JangJuMan@github.io>
2021-02-16 18:46:38 +0900

    주만: 설정탭 아이콘 잠시 제거

--- lib/settings/setting.dart
+++ lib/settings/setting.dart
@@ -51,9 +51,9 @@ class _SettingsState extends State<Settings> {
           // IconButton(
           //   icon: Icon(Icons.logout),
           // )
-          IconButton(
-            icon: Icon(Icons.login_outlined),
-          )
+          // IconButton(
+          //   icon: Icon(Icons.login_outlined),
+          // )
         ],
       ),
       body: _myListView(),
@@ -68,7 +68,7 @@ class _SettingsState extends State<Settings> {
           // 아이콘 내맘대로 한거임 바꿔도 됨!
           _comp_listTile(Icons.event_note, '공지사항', UpdateInfo()),
           _comp_listTile(Icons.person_pin, '내정보', MyInfo()),
-          _comp_listTile(Icons.new_releases_outlined, '버전정보/개발자', Version()),
+          _comp_listTile(Icons.new_releases/*_outlined*/, '버전정보/개발자', Version()),
           _comp_listTile(Icons.notifications_none, '알림', Alarm()),
           _comp_listTile(Icons.bug_report, '버그제보', Bug()),
           _comp_listTile(Icons.lock, '이용약관', TermsOfService()),
